a7947fd9a61432e02ebec68eb0ffa27e501aeaf2
nezroy <nezroy@gmail.com>
2016-05-31 11:05:20 -0600

    Update to latest schema/verdesc

--- defs.js
+++ defs.js
@@ -1,7 +1,7 @@
 module.exports = {
 	FORMAT_ID: 1,
 	SCHEMA: 201604290,
-	VERSION: 201604290,
-	VERDESC: "Citadel 1.1",
+	VERSION: 201605310,
+	VERDESC: "YC 118.5",
 	COPY: "©CCP hf. All rights reserved. Used with permission."
 };
